4d54c50e9914b965d5f61de8bbb915c71c772064
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2018-09-17 14:02:34 -0700

    Playlist Name change working

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -39,6 +39,7 @@ class App extends Component {
       }
       this.addTrack = this.addTrack.bind(this);
       this.removeTrack = this.removeTrack.bind(this);
+      this.updatePlaylistName = this.updatePlaylistName.bind(this);
     }
 
   addTrack(track) {
@@ -57,6 +58,11 @@ class App extends Component {
     this.setState({playlistTracks: updatePlaylist});
   }
 
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
+
+
   render() {
     return (
       <div>
@@ -68,7 +74,7 @@ class App extends Component {
             {/*<!-- Add a SearchResults component -->*/}
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
             {/*<!-- Add a Playlist component -->*/}
-            <PlayList playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
+            <PlayList playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName}/>
           </div>
 
         </div>
--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -3,10 +3,20 @@ import TrackList from '../TrackList/TrackList';
 import './PlayList.css'
 
 class PlayList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    let newName = e.target.value;
+    this.props.onNameChange(newName)
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={"New Playlist"}/>
+        <input defaultValue={"New Playlist"} onChange={this.handleNameChange}/>
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval = {true}/>
         <a className="Playlist-save">SAVE TO SPOTIFY</a>
       </div>
