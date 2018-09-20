f98c954ef9486dd285b0c85fcd4eab8b787c6e4f
rhoffenexitquitgit config --global user.name rhoffen <rebecca.hoffenberg@gmail.com>
2018-09-20 08:37:39 -0700

    Access token working.  Trying to implement search.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,35 +1,49 @@
-import React from 'react';
+//import React from 'react';
 
 const client_id=process.env.REACT_APP_SPOTIFY_CLIENT_KEY;
+const queryRedirect = '&redirect_uri=http://localhost:3000/';
 
-let token;
+let accessToken;
 
 const Spotify = {
-
-  async getAccessToken () {
-    if (token) {
-      return token;
-    } else {
-      const urltoFetch = 'https://accounts.spotify.com/authorize?'
-      const queryResponseType = '&response_type=token';
-      const queryRedirect = '&redirect_uri=http://localhost:3000/';
-      const scopes = '&scope=user-read-private playlist-modify-private'
-      const endpoint = `${urltoFetch}client_id=${client_id}${queryRedirect}${scopes}${queryResponseType}`
-      try{
-        console.log(endpoint)
-        const response = await fetch(endpoint);
-        if(response.ok){
-          console.log(response);
-        }
-      }
-      catch(error){
-        console.log(error)
-      }
+  async getAccessToken() {
+    //If there's an access token, return it.
+    if (accessToken) {
+      return accessToken;
     }
-  },
-
+    //If access token not set yet, check the URL to see if it's there.
+    let tokenArray=window.location.href.match(/access_token=([^&]*)/);
+    let expiresArray=window.location.href.match(/expires_in=([^&]*)/);
+    if (tokenArray && expiresArray) {
+      accessToken=tokenArray[1];
+      let expiresIn=expiresArray[1];
+      window.setTimeout(() => accessToken = '', expiresIn*1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    //If there's no token and expiration in the URL, to to Spotify and get it
+    } else {
+        const urlToLogin = 'https://accounts.spotify.com/authorize?'
+        const queryResponseType = '&response_type=token';
+        const scopes = '&scope=playlist-modify-public';
+        const authEndpoint = `${urlToLogin}client_id=${client_id}${queryRedirect}${scopes}${queryResponseType}`;
+        window.location=authEndpoint;
+      }
+    },
   async search(term) {
     Spotify.getAccessToken();
+    const baseUrl = 'https://api.spotify.com/v1';
+    const searchParams = '/search?type=track&q=';
+    let searchEndpoint = `${baseUrl}${searchParams}${term}`;
+    console.log(`The search endpoint is ${searchEndpoint}`);
+    // await fetch(searchEndpoint,{
+    //   headers: {Authorization: `Bearer ${accessToken}`}
+    //   }
+    // ).then(response => {
+    //   if (response.ok) {
+    //     let jsonResponse = response.json();
+    //     return jsonResponse;
+    //   }
+    // }).then(jsonResponse => {console.log(jsonResponse); console.log(Object.keys(jsonResponse))})
   }
 }
 
